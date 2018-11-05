dd298e34389fbfae3c541678db8921908b846d1a
K. Adam White <kadamwhite@users.noreply.github.com>
2018-11-05 11:58:48 -0500

    Rename featured-item-blocks.php to plugin.php

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@
  * Version:     0.3.1
  * Author:      K.Adam White <adam@kadamwhite.com>
  * Author URI:  http://kadamwhite.com
- * License:     GPL-2.0+
+ * License:     GPL-2.0+ or Artistic License 2.0
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 require_once( __DIR__ . '/inc/asset-loader.php' );
