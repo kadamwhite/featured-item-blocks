de40927ea8f7994ce121ee6ce779ac0b77de751c
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-21 23:56:46 -0400

    Upgrade build to use webpack-helpers

--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -1,112 +1,48 @@
 /**
  * This file defines the configuration for development and dev-server builds.
  */
-const { unlinkSync } = require( 'fs' );
-const onExit = require( 'signal-exit' );
-const webpack = require( 'webpack' );
-const HardSourceWebpackPlugin = require( 'hard-source-webpack-plugin' );
-const ManifestPlugin = require( 'webpack-manifest-plugin' );
+const { resolve } = require( 'path' );
+const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
+const { choosePort, cleanOnExit, filePath } = helpers;
 
-const { devServerPort, externals, filePath, loaders, stats } = require( './config-utils' );
+const pluginPath = ( ...pathParts ) => resolve( __dirname, '..', ...pathParts );
 
-// Clean up manifest on exit.
-onExit( () => {
-	try {
-		unlinkSync( 'build/asset-manifest.json' );
-	} catch ( e ) {
-		// Silently ignore unlinking errors: so long as the file is gone, that is good.
-	}
-} );
+// Clean up manifests on exit.
+cleanOnExit( [
+	pluginPath( 'build/asset-manifest.json' ),
+] );
 
-const port = devServerPort();
-const publicPath = `http://localhost:${ port }/build/`;
-
-/**
- * Theme development build configuration.
- */
-module.exports = {
-	mode: 'development',
-	devtool: 'cheap-module-source-map',
-	context: process.cwd(),
-	// Allow config to override shared devServer properties.
-	devServer: {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-		},
-		hotOnly: true,
-		watchOptions: {
-			aggregateTimeout: 300,
-		},
-		stats,
-		port,
-	},
-	optimization: {
-		nodeEnv: 'development',
-	},
+const config = {
+	externals,
 	entry: {
-		editor: filePath( 'src', 'index.js' ),
+		editor: pluginPath( 'src/index.js' ),
 	},
 	output: {
-		// Add /* filename */ comments to generated require()s in the output.
-		pathinfo: false,
-		path: filePath( 'build' ),
-		filename: '[name].js',
-		publicPath,
+		path: pluginPath( 'build' ),
 	},
-	externals,
-	module: {
-		strictExportPresence: true,
-		rules: [
-			// First, run the linter before Babel processes the JS.
-			loaders.eslint,
-			{
-				// "oneOf" will traverse all following loaders until one will
-				// match the requirements. If no loader matches, it will fall
-				// back to the "file" loader at the end of the loader list.
-				oneOf: [
-					// Inline any assets below a specified limit as data URLs to avoid requests.
-					loaders.url,
-					// Process JS with Babel.
-					loaders.js,
-					{
-						test: /\.scss$/,
-						use: [
-							require.resolve( 'style-loader' ),
-							// Process SASS into CSS.
-							loaders.css,
-							loaders.postcss,
-							loaders.sass,
-						],
-					},
-					// "file" loader makes sure any non-matching assets still get served.
-					// When you `import` an asset you get its filename.
-					loaders.file,
-				],
-			},
-		],
-	},
-	plugins: [
-		// Generate a manifest file which contains a mapping of all asset filenames
-		// to their corresponding output file so that PHP can pick up their paths.
-		new ManifestPlugin( {
-			fileName: 'asset-manifest.json',
-			writeToFileEmit: true,
-			publicPath,
-		} ),
-		// Enable HMR.
-		new webpack.HotModuleReplacementPlugin(),
-		// Faster rebuilds.
-		new HardSourceWebpackPlugin( {
-			cacheDirectory: filePath( 'node_modules', '.cache', 'hard-source', '[confighash]' ),
-			info: {
-				level: 'warn',
-			},
-			cachePrune: {
-				// Only delete caches older than two days.
-				maxAge: 2 * 24 * 60 * 60 * 1000,
-				// Only delete caches if cache folder is > 50mb.
-				sizeThreshold: 50 * 1024 * 1024,
-			},
-		} ),
-	],
 };
+
+if (
+	process.argv[1].indexOf( 'webpack-dev-server' ) !== -1
+	&& filePath( '.config' ) === __dirname
+) {
+	// Webpack DevServer is being run from within this project: automatically
+	// detect & bind to an open port.
+	const cwdRelativePublicPath = ( path, port ) => `http://localhost:${ port }${ path.replace( process.cwd(), '' ) }/`;
+	module.exports = choosePort( 9090 ).then( port => presets.development( {
+		...config,
+		devServer: {
+			port,
+		},
+		output: {
+			...config.output,
+			publicPath: cwdRelativePublicPath( config.output.path, port ),
+		},
+	} ) );
+} else if ( filePath( '.config' ) === __dirname ) {
+	// Dev-mode static file build is being run from within this project.
+	module.exports = presets.development( config );
+} else {
+	// This configuration is being injested by a parent project's build process.
+	module.exports = config;
+}
